b68eceb07d544591ddafaea9ed57067496478151
Ethan Gaebel <2490298+egaebel@users.noreply.github.com>
2023-01-12 16:04:08 -0800

    Add some comments to WowView

--- src/WowView.js
+++ src/WowView.js
@@ -1,6 +1,9 @@
 import React from "react";
 
 function WowView(props) {
+    // LESSON: Notice how we pass a lambda function to the onClick parameter. 
+    //          Notice that whenever you click a wow it will print out props.fullContent 
+    //          to the javascript console in the browser developer tools.
     return (<tr onClick={() => console.log("Click received on: '%s'", props.fullContent)}><pre>{props.fullContent}</pre></tr>)
 }
 
